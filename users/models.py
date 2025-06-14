be0b9aa3debf7d630aebb8be7e8ceb20c7c4b64a
Dmitrii Grechko <prime.optimys@mail.ru>
2025-06-14 09:32:23 +0500

    add fields avatar, phone, country in model User edit users/forms.py

--- users/models.py
+++ users/models.py
@@ -6,9 +6,18 @@ from django.contrib.auth.models import Group
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(upload_to="avatars/", verbose_name="Аватар",
+                               blank=True, null=True,
+                               help_text="Звгрузите свой аватар")
+    phone = models.CharField(max_length=35, verbose_name="Телефон", blank=True,
+                             null=True, help_text="Введите номер телефона")
+    country = models.CharField(max_length=65, verbose_name="Страна",
+                               blank=True, null=True,
+                               help_text="Введите страну")
     token = models.CharField(
         max_length=100, verbose_name="Токен", blank=True, null=True
     )
+
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
